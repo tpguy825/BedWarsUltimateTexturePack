10e3f479d2e2707cd85b3599a252a01d32d679cb
tpguy825 <75158953+tpguy825@users.noreply.github.com>
2021-06-14 16:43:36 +0100

    Update prepare-dir-mcpack.sh

--- build/prepare-dir-mcpack.sh
+++ build/prepare-dir-mcpack.sh
@@ -4,6 +4,6 @@ $newdirname = ./texture-packs
 if [ -d "$newdirname" ]; then
 echo "Directory already exists" ;
 else
-`mkdir -p $newdirname`;
+mkdir -p $newdirname
 echo "$newdirname directory is created"
 fi
